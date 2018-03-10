3989cbf59f25d0c4b0df1a3509bb73056e3d52bc
Val Bancer <valerij.bancer@gmail.com>
2018-03-10 22:35:59 +0100

    refactor: Convert var to let declarations

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,5 +1,5 @@
 let restaurant;
-var map;
+let map;
 
 /**
  * Initialize Google map, called from HTML.
@@ -11,13 +11,13 @@ window.initMap = () => {
     if (error) { // Got an error!
       console.error(error);
     } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
+      map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
         center: restaurant.latlng,
         scrollwheel: false
       });
       fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+      DBHelper.mapMarkerForRestaurant(self.restaurant, map);
     }
   });
 };
