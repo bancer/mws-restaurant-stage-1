edd9452f1cef6b4f77a5fc41c967c850d4c278c8
Val Bancer <valerij.bancer@gmail.com>
2018-03-26 22:46:27 +0200

    feat: Add alt property to img tags

--- js/main.js
+++ js/main.js
@@ -164,6 +164,7 @@ const createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + ' restaurant photo';
   li.append(image);
 
   const name = document.createElement('h1');
