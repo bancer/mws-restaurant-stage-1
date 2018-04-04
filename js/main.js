b671a264201202781e8c09c208cf5589b6509153
Val Bancer <valerij.bancer@gmail.com>
2018-04-04 23:15:34 +0200

    feat: create service worker that delivers cached requests
    * feat: create service worker that delivers cached requests
    
    * chore: adjust javascript array code style

--- js/main.js
+++ js/main.js
@@ -222,3 +222,15 @@ const addMarkersToMap = (restaurants = self.restaurants) => {
     markers.push(marker);
   });
 };
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
