5663a42a5274cae0822044a99cf1759c31307a61
Val Bancer <valerij.bancer@gmail.com>
2018-04-04 22:51:50 +0200

    feat: create service worker that delivers cached requests

---
+++ sw.js
@@ -0,0 +1,54 @@
+const staticCacheName = 'mws-restaurant-stage-v1';
+const allCaches = [
+  staticCacheName
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(function(cache) {
+        console.log('Opened cache');
+        const urlsToCache = [
+          '/',
+          '/css/styles.css',
+          '/js/dbhelper.js',
+          '/js/main.js',
+          '/js/restaurant_info.js'
+        ];
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+            return cacheName.startsWith('mws-restaurant-') &&
+                 !allCaches.includes(cacheName);
+          }).map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
