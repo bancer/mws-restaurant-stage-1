b49e102a0fd409b944b685f116df9fc4c4dbc757
Val Bancer <valerij.bancer@gmail.com>
2018-06-03 19:33:54 +0200

    feat: added manifest.json (#26)

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -205,3 +205,14 @@ const getParameterByName = (name, url) => {
   }
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 };
+
+// Registers the service worker, see https://developers.google.com/web/fundamentals/primers/service-workers/
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
